dbbee6940c4633f3614fde76683c3edc92805b0f
anoChick <anoinchick@gmail.com>
2021-02-26 23:56:25 +0900

    自動デプロイ追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+  <Layout title="猪苗代銀行">
+    <h1>猪苗代銀行deploy</h1>
     <p>
       <Link href="/about">
         <a>About</a>
