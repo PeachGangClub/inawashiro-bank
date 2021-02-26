8ab102f39b4680ce5eb0b64f653beb9e4ef1ab68
anoChick <anoinchick@gmail.com>
2021-02-27 00:15:40 +0900

    タイトルにstyle適用

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,17 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="猪苗代銀行">
-    <h1>猪苗代銀行deploy</h1>
+    <h1 className="title">猪苗代銀行</h1>
     <p>
       <Link href="/about">
         <a>About</a>
       </Link>
     </p>
+    <style jsx>{`
+      .title {
+        color: ${'pink'}
+      }
+    `}</style>
   </Layout>
 )
 
